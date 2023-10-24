eeb8afb451478aff11b982f6a25366c1c994b470
sinhnn <sinhnn.92@gmail.com>
2023-10-24 10:01:29 +0700

    Init code

---
+++ README.md
@@ -0,0 +1,8 @@
+# Skip-Ads
+Skip-Ads is an simple extension skip ads automatically.
+It does not mean blocking ads.
+
+# Install
+Clone this repository or download as zip then extract it.
+Then following loading unpackage extension instruction
+https://developer.chrome.com/docs/extensions/mv3/getstarted/development-basics/#load-unpacked
---
+++ js/index.js
@@ -0,0 +1,60 @@
+
+
+let ogVolume = 1;
+let pbRate = 1;
+
+function skipAdsOnSelectors(selectors, callback)
+{
+    selectors.map(selector => { Array.from(document.body.querySelectorAll(selector)).forEach(item => callback(item));});
+}
+
+
+function skipAdsOnSelectorFunc(selectorFunc, callback)
+{
+    selectorFunc().map(selector => { Array.from(document.body.querySelectorAll(selector)).forEach(item => callback(item));});
+}
+
+function skipAds ()
+{
+    let ad = document.getElementsByClassName("video-ads ytp-ad-module")[0];
+    let vid = document.getElementsByClassName("video-stream html5-main-video")[0];
+
+    if (ad == undefined) { pbRate = vid.playbackRate; }
+
+    const closeBtnSelectors = [
+        '[class*="ytp-ad-overlay-close-button"]',
+        '[class*="ytp-ad-text ytp-ad-skip-button-text"]',
+    ];
+    skipAdsOnSelectors(closeBtnSelectors, (item) => item.click());
+
+    const sideAdSelectors = [
+        '[class*="style-scope ytd-watch-next-secondary-results-renderer sparkles-light-cta GoogleActiveViewElement"]',
+        '[class*="style-scope ytd-item-section-renderer sparkles-light-cta"]',
+        '[class*="ytp-ad-message-container"]',
+    ];
+    skipAdsOnSelectors(sideAdSelectors, (item) => item.style.display = "none");
+    
+    const removeSelectors = [
+        '[class*="style-scope ytd-companion-slot-renderer"]'
+    ];
+    skipAdsOnSelectors(removeSelectors, (item) => item.remove());
+
+    if (ad !== undefined) {
+        if (ad.children.length > 0) {
+            if (document.getElementsByClassName("ytp-ad-text ytp-ad-preview-text")[0] !== undefined) {
+                vid.playbackRate = 16;
+                vid.muted = true;
+            }
+        }
+    }
+}
+
+/* TODO: execute skip after navigation event */
+let isBusy = false;
+const m_skipAdsInterval = setInterval(() => {
+    if (isBusy ) return;
+    isBusy = true;
+    skipAds();
+    isBusy = false;
+}, 5000);
+
